65fe42bff987aed50ee8b569b848dba94d0ac786
wittyneko <wittytutu@gmail.com>
2017-07-12 18:48:29 +0800

    remove auto-layout

--- auto-layout/src/main/java/cn/wittyneko/autolayout/helper/AutoLayoutParams.java
+++
@@ -1,9 +0,0 @@
-package cn.wittyneko.autolayout.helper;
-
-/**
- * 布局信息接口
- * Created by Brady on 2016/5/2.
- */
-public interface AutoLayoutParams {
-    AutoLayoutInfo getUniversalLayoutInfo();
-}
